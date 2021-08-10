480ec3b5738302659d61362498e717153c70e790
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2021-08-10 11:49:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,31 @@ function preload() {
     
     function setup() {
     canvas=createCanvas(300,300);
-    canvas.center()
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
     }
 
+function modelLoaded() {
+    console.log("poseNet is loaded");
+}
+
     function draw() {
-        
+    image(video,0,0,300,300);   
     }
      function take_snapshot() {
     save("my_filter_image.png");
+    }
+
+    function gotPoses(results) {
+        if(results.length>0){
+            console.log(results);
+            noseX=results[0].pose.nose.x;
+            noseY=results[0].pose.nose.y;
+            console.log("nose x="+noseX);
+            console.log("nose y="+noseY);
+        }
     }
