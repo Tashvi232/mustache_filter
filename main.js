8703019983766176dd255f722b257566986702fb
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2021-08-15 12:56:31 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
-    
+   mustache=loadImage(" https://i.postimg.cc/3x3QzSGq/m.png") 
     }
     
     function setup() {
@@ -17,8 +20,10 @@ function modelLoaded() {
 }
 
     function draw() {
-    image(video,0,0,300,300);   
+    image(video,0,0,300,300);
+    image(mustache,noseX,noseY,80,80);   
     }
+
      function take_snapshot() {
     save("my_filter_image.png");
     }
@@ -26,8 +31,8 @@ function modelLoaded() {
     function gotPoses(results) {
         if(results.length>0){
             console.log(results);
-            noseX=results[0].pose.nose.x;
-            noseY=results[0].pose.nose.y;
+            noseX=results[0].pose.nose.x-35;
+            noseY=results[0].pose.nose.y-15;
             console.log("nose x="+noseX);
             console.log("nose y="+noseY);
         }
